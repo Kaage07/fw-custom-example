fdbf2515a997496fe183306084bdc7c8a72f61f5
Osman Samet Şenol <92851088+Kaage07@users.noreply.github.com>
2026-08-26 15:14:39 +0300

    Update board_configuration.cpp

--- boards/board1/board_configuration.cpp
+++ boards/board1/board_configuration.cpp
@@ -13,6 +13,15 @@ Gpio getWarningLedPin() {
 	return Gpio::Unassigned;
 }
 
+static void setupSdCard() {
+	engineConfiguration->sdCardSpiDevice = SPI_DEVICE_3;
+	engineConfiguration->sdCardCsPin = Gpio::D2;
+
+	engineConfiguration->is_enabled_spi_3 = true;
+	engineConfiguration->spi3sckPin = Gpio::C10;
+	engineConfiguration->spi3misoPin = Gpio::C11;
+	engineConfiguration->spi3mosiPin = Gpio::C12;
+
 void setup_custom_board_overrides() {
 
 }
